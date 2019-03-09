e647732086d6c441b88694caa57f71b60bbcbf63
Brayconn <29030756-Brayconn@users.noreply.github.com>
2019-03-08 20:30:21 -0800

    Fixed a typo

--- FileMerger/Properties/AssemblyInfo.cs
+++ FileMerger/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FileMerger")]
-[assembly: AssemblyDescription("Allowes for editing of many files at once by merging them together.")]
+[assembly: AssemblyDescription("Allows for editing of many files at once by merging them together.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brayconn")]
 [assembly: AssemblyProduct("FileMerger")]
